fa348ac2d5ff812f61bad5857ef8ca76eddcf8d8
Roni Sinaga <dragonif01@gmail.com>
2023-05-23 09:44:15 +0700

    First Commit

--- org.astina.mfg.plugin/src/org/astina/mfg/plugin/factory/InvoiceInfoMfgFactory.java
+++ org.astina.mfg.plugin/src/org/astina/mfg/plugin/factory/InvoiceInfoMfgFactory.java
@@ -21,13 +21,13 @@ public class InvoiceInfoMfgFactory implements IInfoFactory {
 		InfoPanel info = null;
 		info = new InfoInvoiceWindow(WindowNo, tableName, keyColumn, value, multiSelection, whereClause, AD_InfoWindow_ID, lookup);
     	if (!info.loadedOK()) {
-            info = new InfoGeneralPanel (value, WindowNo,
+            /*info = new InfoGeneralPanel (value, WindowNo,
                 tableName, keyColumn,
                 Boolean.TRUE, whereClause, lookup);
         	if (!info.loadedOK()) {
         		info.dispose(false);
         		info = null;
-        	}
+        	}*/
     	}
     	MInfoWindow checkEntity = new Query(Env.getCtx(),MInfoWindow.Table_Name,MInfoWindow.COLUMNNAME_AD_InfoWindow_ID+"=?",null)
     		.setParameters(AD_InfoWindow_ID)
@@ -47,9 +47,9 @@ public class InvoiceInfoMfgFactory implements IInfoFactory {
 			InfoPanel info = null;
 			info = new InfoInvoiceWindow(lookup.getWindowNo(), tableName, keyColumn, value, Boolean.TRUE, whereClause, AD_InfoWindow_ID, Boolean.TRUE);
 	    	if (!info.loadedOK()) {
-	            info = new InfoGeneralPanel (value, -1,
+	            /*info = new InfoGeneralPanel (value, -1,
 	                tableName, keyColumn,
-	                Boolean.TRUE, whereClause, Boolean.TRUE);
+	                Boolean.TRUE, whereClause, Boolean.TRUE);*/
 	        	if (!info.loadedOK()) {
 	        		info.dispose(false);
 	        		info = null;
@@ -74,6 +74,13 @@ public class InvoiceInfoMfgFactory implements IInfoFactory {
 			return (InfoWindow) info;
 		else
 			return null;
+	}
+
+	@Override
+	public InfoPanel create(int WindowNo, String tableName, String keyColumn, String value, boolean multiSelection,
+			String whereClause, int AD_InfoWindow_ID, boolean lookup, GridField field) {
+		// TODO Auto-generated method stub
+		return null;
 	}
  
 }
